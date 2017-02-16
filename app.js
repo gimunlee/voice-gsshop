384f1ba242c7e1cbe92badc70d6a78d879210c4e
이종서 사원 신기술개발팀 (westbell@lgcns.com <79970@CORP.LGCNS.COM>
2017-02-16 14:12:20 +0900

    api.ai template2

--- app.js
+++ app.js
@@ -1,112 +1,51 @@
+
 // Copyright 2016, Google, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the 'License');
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//   http://www.apache.org/licenses/LICENSE-2.0
+//    http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an 'AS IS' BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// [START app]
 'use strict';
 
 process.env.DEBUG = 'actions-on-google:*';
-
-let ActionsSdkAssistant = require('actions-on-google').ActionsSdkAssistant;
+let Assistant = require('actions-on-google').ApiAiAssistant;
 let express = require('express');
 let bodyParser = require('body-parser');
 
 let app = express();
-app.set('port', (process.env.PORT || 8080));
 app.use(bodyParser.json({type: 'application/json'}));
 
-var homerun = [4, 5, 6];
-var number = [0, 0, 0];
+// [START YourAction]
+app.post('/', function (req, res) {
+  const assistant = new Assistant({request: req, response: res});
+  console.log('Request headers: ' + JSON.stringify(req.headers));
+  console.log('Request body: ' + JSON.stringify(req.body));
 
-app.post('/', function (request, response) {
-  console.log('handle post');
-  const assistant = new ActionsSdkAssistant({request: request, response: response});
-  const NUMBER_ARGUMENT = 'number';
-
-  function mainIntent (assistant) {
-    console.log('mainIntent');
-    let inputPrompt = assistant.buildInputPrompt(true, '<speak>L.G. C.N.S  let\'s play number baseball' );
-    // let inputPrompt = assistant.buildInputPrompt(true, '<speak>Hi! <break time="1"/> ' +
-    //       'I can read out an ordinal like ' +
-    //       '<say-as interpret-as="ordinal">123</say-as>. Say a number.</speak>',
-    //       ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
-    assistant.ask(inputPrompt);
+  // Fulfill action business logic
+  function responseHandler (assistant) {
+    // Complete your fulfillment logic and send a response
+    assistant.tell('Hello, World!');
   }
 
-  function rawInput (assistant) {
-    var inputPrompt;
-    console.log('rawInput');
-    if (assistant.getRawInput() === 'bye') {
-      assistant.tell('Goodbye!');
-    } else {
-      inputPrompt = assistant.buildInputPrompt(true, '<speak>Okay, you gave me a raw input.');
-      assistant.ask(inputPrompt);
-      for (var i = 0; i < 3; i++) {
-        number[i] = assistant.getArgument(NUMBER_ARGUMENT);
-      }
-
-      assistant.tell('You said' + number1 + number2 + number3);
-
-      var strike = 0;
-      var ball = 0;
-
-      for (var i = 0; i < 3; i++) {
-        if (number[i] == homerun[i]) strike++;
-      }
-
-      for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3; j++) {
-          if (number[i] == homerun[i]) {
-            ball++;
-            break;
-          }
-        }
-      }
-      
-      ball -= strike;
-
-       if (strike == 3) {
-         assistant.tell('Congratulations. Home Run.');
-       } else if (strike == 0 && ball == 0) {
-         assistant.tell('I\'m sorry, it\'s  out.');
-         let inputPrompt  = assistant.buildInputPrompt(true, '<speak>Out.');
-       } else {
-         let inputPrompt  = assistant.buildInputPrompt(true, '<speak>' + strike + 'strike, ' + ball + 'ball.');
-       }
-
-      // assistant.ask(inputPrompt);
-      
-      // let inputPrompt = assistant.buildInputPrompt(true, '<speak>You said, <say-as interpret-as="ordinal">' +
-      //   assistant.getRawInput() + '</say-as></speak>',
-      //     ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
-      // assistant.ask(inputPrompt);
-    }
-  }
-
-  let actionMap = new Map();
-  actionMap.set(assistant.StandardIntents.MAIN, mainIntent);
-  actionMap.set(assistant.StandardIntents.TEXT, rawInput);
-
-  assistant.handleRequest(actionMap);
+  assistant.handleRequest(responseHandler);
 });
+// [END YourAction]
 
-app.get("/", (req, res)=> {
-  res.send("Hello, google");
-})
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  let server = app.listen(process.env.PORT || 8080, function () {
+    let port = server.address().port;
+    console.log('App listening on port %s', port);
+  });
+  // [END server]
+}
 
-// Start the server
-let server = app.listen(app.get('port'), function () {
-  console.log('App listening on port %s', server.address().port);
-  console.log('Press Ctrl+C to quit.');
-});
-// [END app]
+module.exports = app;
